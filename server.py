789f12bc48029090156272fe81c0cae4b401bc49
pnavab <114110926+pnavab@users.noreply.github.com>
2023-11-30 15:39:17 -0800

    add frontend

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import random
 
 app = FastAPI()
+
+origins = ["*"]  # You can replace "*" with your frontend domain(s)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
